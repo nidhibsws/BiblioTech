c8f4bc6f3065d96e9931f601229ae393b4b15139
Nidhi Biswas <85760371+nidhibsws@users.noreply.github.com>
2024-12-22 19:10:26 -0500

    Update drop_tables.sh

--- shell_prompt/drop_tables.sh
+++ shell_prompt/drop_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64
-"a23zafar/07099589@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.scs.ryerson.ca)
+"*****/*****@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.scs.ryerson.ca)
 (Port=1521))(CONNECT_DATA=(SID=orcl12c)))" <<EOF
 DROP TABLE AUTHOR CASCADE CONSTRAINTS;
 DROP TABLE BOOK CASCADE CONSTRAINTS;
