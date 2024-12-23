9df63e5067aff84d331ba821505711a420b54f69
Nidhi Biswas <85760371+nidhibsws@users.noreply.github.com>
2024-12-22 19:11:02 -0500

    Update queries.sh

--- shell_prompt/queries.sh
+++ shell_prompt/queries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64
-"a23zafar/07099589@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.scs.ryerson.ca)
+"*****/*****@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.scs.ryerson.ca)
 (Port=1521))(CONNECT_DATA=(SID=orcl12c)))" <<EOF
 SELECT student_name FROM student ORDER BY student_name; #List the names of all current students of the library in alphabetical order
 
